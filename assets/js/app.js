7c044c4011af0c6d0e87a8553240f6d7686be604
Angela Adams <aadams3867@gmail.com>
2016-08-09 20:52:10 -0400

    added js

---
+++ assets/js/app.js
@@ -0,0 +1,29 @@
+//var dataRef = firebase.database();
+
+// Initial Values
+var empName = "";
+var role = "";
+var startDate = 0;
+var monthlyRate = 0;
+
+// Capture Button Click
+$("button").on("click", function() {
+
+	empName = $('#empName').val().trim();
+	role = $('#role').val().trim();
+	startDate = $('#startDate').val().trim();
+	monthlyRate = $('#monthlyRate').val().trim();
+
+	console.log("Submit button pushed!");
+
+/*	// Code for the push
+	dataRef.ref().push({
+		name: empName,
+		role: role,
+		startDate: startDate,
+		monthlyRate: monthlyRate,
+	});*/
+
+	// Don't refresh the page!
+	return false;
+});
